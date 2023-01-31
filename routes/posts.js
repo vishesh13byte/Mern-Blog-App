3dfb647ce2555504997e100f2bf9121efb7548db
vishesh13byte <visheshrustagi8@gmail.com>
2023-02-01 02:04:10 +0530

    changed server

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,9 @@ import express from 'express';
 import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
-
+// router.get('/',(req,res)=>{
+//     res.send("THIS WORKS!");
+// });
 router.get('/', getPosts);
 router.post('/', createPost);
 router.get('/:id', getPost);
