b7aa79b74fd8874b1b010c14f50e092aff4bd455
vishesh13byte <visheshrustagi8@gmail.com>
2023-02-01 01:59:50 +0530

    final

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vishu:bbkivines190@cluster0.j7hf3y3.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://vish:bbkivines190@cluster0.j7hf3y3.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
