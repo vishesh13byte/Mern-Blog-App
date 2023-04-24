f25b97a12663e53e40399ada8ae0e981615cc718
vishesh13byte <visheshrustagi8@gmail.com>
2023-04-24 19:20:02 +0530

    first commit

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -6,6 +7,7 @@ import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
+const DATABASE = process.env.DATABASE;//
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -14,7 +16,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://vishu:bbkivines190@cluster0.j7hf3y3.mongodb.net/?retryWrites=true&w=majority';
+const CONNECTION_URL = DATABASE;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
